b07f92bb9dd9a80359e7bc59409b5b77bdc27bed
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-10-21 09:09:41 +0200

    Update twosum1.c

--- 1.Two Sum/twosum1.c
+++ 1.Two Sum/twosum1.c
@@ -3,10 +3,10 @@
 
 // Function to find two indices such that their values add up to the target
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result = (int*)malloc(2 * sizeof(int));       // Allocate memory for the result (2 indices)
-    for (int i=0; i<numsSize; i++) {                   // Iterate through each element of the array
+    int* result = (int*)malloc(2 * sizeof(int));       // Allocate memory for the indices
+    for (int i=0; i<numsSize; i++) {                   // Iterate through each element of the given array
         for (int j= i+1; j<numsSize; j++) {            // For each element, check if there is another element that adds up to the target -> O(n^2) time complexity -> must be done better
-            if (nums[i] + nums[j] == target) {         // If the sum of nums[i] and nums[j] equals the target, store the indices
+            if (nums[i] + nums[j] == target) {         // If the sum of nums[i] and nums[j] = target, store the indices
                 result[0]=i;
                 result[1]=j;
                 *returnSize=2;                         // Set the return size to 2 (since we're returning two indices)
