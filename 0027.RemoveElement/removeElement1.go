6dc06edcfbe0dd5a4e3b87285cc3e4d8617f3b78
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2024-12-25 19:25:02 +0100

    Update removeElement1.go

--- 0027.RemoveElement/removeElement1.go
+++ 0027.RemoveElement/removeElement1.go
@@ -1,12 +1,11 @@
 func removeElement(nums []int, val int) int {
-    //It does not matter what you leave beyond the returned k (hence they are underscores). 
-                                        //Very similar to the previous exercise
-    k := 0                              //But in this case there might not be any elements that match the one you are looking for
+                                       //Very similar to the previous exercise
+    k := 0                             //But in this case there might not be any elements that match the one you are looking for
     for i := 0; i < len(nums); i++ {
         if nums[i] != val {            
             nums[k] = nums[i]          //Place the non-val element at index k
             k++
         }
     }
-    return k
+    return k                           //It does not matter what you leave beyond the returned k (hence they are underscores). 
 }
