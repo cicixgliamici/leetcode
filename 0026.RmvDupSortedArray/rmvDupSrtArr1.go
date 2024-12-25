9777c0c31859f7ac772d57403f18350a13640637
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2024-12-25 18:48:00 +0100

    Create rmvDupSrtArr1.go

---
+++ 0026.RmvDupSortedArray/rmvDupSrtArr1.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) == 0 {                      //Check empty array
+        return 0
+    }                                        //First element is always unique
+    k := 1                                   //Concise notation for declaring variables in GO, automatically infers the type; it will be our "pointer" to the last unique number we have seen
+    for i := 1; i < len(nums); i++ {
+        if nums[i] != nums[k-1] {            //If the next number isn't the same as the last unique number
+            nums[k] = nums[i]                //Update the position of the next unique element
+            k++                              //And increment the counter of the unduplicated numbers
+        }
+    }
+    return k
+}
