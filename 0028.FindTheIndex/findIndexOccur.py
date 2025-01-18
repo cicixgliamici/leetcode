c63976cecd0f50a334275936434dd2d127b017ad
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-01-18 17:09:36 +0100

    Update findIndexOccur.py

--- 0028.FindTheIndex/findIndexOccur.py
+++ 0028.FindTheIndex/findIndexOccur.py
@@ -25,24 +25,3 @@ class Solution:
 
         # If no match was found, return -1.
         return -1
-
-
-"""
-Algorithmic Complexity and Data Structures:
-
-- Time Complexity: O(n*m), where:
-  n is the length of haystack.
-  m is the length of needle.
-
-  In the worst case, we might compare almost every substring of length m in 
-  haystack with needle. Each substring comparison can take O(m) in the worst
-  case, leading to an overall O(n*m) time complexity.
-
-- Space Complexity: O(1). We are not using any additional data structure 
-  proportional to n or m. We are only performing string slicing and comparisons.
-
-- Algorithm Used: This is the naive substring search algorithm. More efficient 
-  algorithms for substring search include Knuth-Morris-Pratt (KMP), Rabin-Karp, 
-  or Boyer-Moore, but for many practical cases (or smaller input sizes), this 
-  straightforward approach is sufficient.
-"""
